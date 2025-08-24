3d94ea37e0bc7af5fb5e3ee93e7acfbc7c26701d
Aadi-0911 <adityap0091@gmail.com>
2025-08-24 17:08:17 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+# Python program to check if a number is even or odd
+
+# Take input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
